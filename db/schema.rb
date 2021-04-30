43673a7a33a9910a074fd7f1bfb3fc43a55b9bd8
toshiki-o23 <tsk.okmt.15@gmail.com>
2021-04-30 19:24:46 +0900

    [Add] profile_photo

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_30_094102) do
+ActiveRecord::Schema.define(version: 2021_04_30_102301) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2021_04_30_094102) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "username"
+    t.string "profile_photo", default: "default_profile_img.png"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
